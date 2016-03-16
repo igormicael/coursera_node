0eb4167b723946775414876a0376726a809ac6eb
igor micael <igor.m.peixoto@gmail.com>
2016-03-15 21:41:02 -0300

    usage of local modules and yargs module

--- rect.js
+++ rect.js
@@ -1,6 +1,20 @@
-exports.perimeter = function(x, y) {
-    return (2 * (x + y));
-}
-exports.area = function(x, y) {
-    return (x * y);
+module.exports = function(x,y,callback) {
+  try {
+    if (x < 0 || y < 0) {
+        throw new Error("Rectangle dimensions should be greater than zero: l = "
+                            + x + ", and b = " + y);
+    }
+    else
+         callback(null, {
+            perimeter: function () {
+        		   return (2*(x+y));
+			},
+            area:function () {
+        		    return (x*y);
+			}
+    });
+  }
+  catch (error) {
+        callback(error,null);
+  }
 }
--- simplerect.js
+++ simplerect.js
@@ -1,16 +1,23 @@
 var rect = require("./rect.js")
 
+var argv = require('yargs')
+    .usage('Usage: node $0 --l=[num] --b=[num]')
+    .demand(['l','b'])
+    .argv;
+
 function solveRect(l, b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
-    if (l < 0 || b < 0) {
-        console.log("Rectangle dimensions should be greater than zero:  l = " + l + ",  and b = " + b);
-    } else {
-        console.log("The area of a rectangle of dimensions length = " + l + " and breadth = " + b + " is " + rect.area(l, b));
-        console.log("The perimeter of a rectangle of dimensions length = " + l + " and breadth = " + b + " is " + rect.perimeter(l, b));
-    }
+    rect(l,b, function(err,rectangle){
+      if(err){
+        console.log(err);
+      }else{
+        console.log("The area of a rectangle of dimensions length " +
+          " = " + l + " and breadth = " + b + " is " + rectangle.area());
+        console.log("The perimeter of a rectangle of dimensions " +
+          "length = " + l + " and breadth = " + b + " is " + rectangle.perimeter());
+      }
+    })
 }
 
-solveRect(2, 4);
-solveRect(3, 5);
-solveRect(-3, 5);
+solveRect(argv.l,argv.b);
