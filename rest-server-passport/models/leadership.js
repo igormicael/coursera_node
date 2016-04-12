8b7e64cdc5bcf46028af2d0fb1e37209bc82eb02
igor micael <igor.m.peixoto@gmail.com>
2016-04-12 19:41:06 -0300

    assignment 4

--- rest-server-passport/models/leadership.js
+++ rest-server-passport/models/leadership.js
@@ -2,9 +2,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-require('mongoose-currency').loadType(mongoose);
-var Currency = mongoose.Types.Currency;
-
 // create a schema
 var leadershipSchema = new Schema({
     name: {
